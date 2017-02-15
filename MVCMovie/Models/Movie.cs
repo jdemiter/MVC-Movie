a108e792c054fee3d3e4ef3fb2874e6332d56469
jdemiter <jdemiter@W766297>
2017-02-14 19:38:20 -0700

    Initial project insertion

---
+++ MVCMovie/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCmovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Required]
+        public Decimal Price { get; set; }
+
+
+
+        
+
+
+
+    }
+}
